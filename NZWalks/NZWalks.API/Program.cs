28b90c9561d70f08ea867f80888b4f13129ffebd
magrile <marcgrinolechado@gmail.com>
2022-11-03 12:14:18 +0100

    End Of Section 2

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Injeccion de la dependencia de la BBDD
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
+
 
 var app = builder.Build();
 
